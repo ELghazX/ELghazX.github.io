997feb6c06d12e65b8782f5b7c8beaa8b44508ce
Muhammad Ghazali <152017206+muhammadghazali69@users.noreply.github.com>
2024-09-24 14:49:24 +0800

    posttest3

---
+++ posttest3/scripts/script.js
@@ -0,0 +1,35 @@
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+let lightMode = localStorage.getItem('light-mode');
+const temaChange = document.getElementById('tema-change');
+
+temaChange.addEventListener('click', () => {
+    lightMode !== 'active' ? enableLightMode() : disableLightMode();
+    alert("Tema berubah");
+});
+
+const enableLightMode = () => {
+    document.body.classList.add('light-mode');
+    localStorage.setItem('light-mode', 'active');
+    lightMode = localStorage.getItem('light-mode');
+};
+
+const disableLightMode = () => {
+    document.body.classList.remove('light-mode');
+    localStorage.setItem('light-mode', null);
+    lightMode = localStorage.getItem('light-mode');
+};
+
+document.querySelector('.chatadmin').addEventListener('click', () => {
+    alert('Fitur belum tersedia');
+});
